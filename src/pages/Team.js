d591b30510704dbe4389fa623b831fc891f51ee0
vampire hunter <vampirehunter@vampires-MacBook-Pro.local>
2022-04-29 21:46:13 -0700

    team update

--- src/pages/Team.js
+++ src/pages/Team.js
@@ -13,12 +13,17 @@ function Team() {
         )
         else if (index == 1) return (
             <svg viewBox="0 0 100 50">
+                <rect x="30" y="14" width="30" height="30" transform="rotate(-45, 40 20)" stroke="#FFFFFF" />
+            </svg>
+        )
+        else if (index == 2) return (
+            <svg viewBox="0 0 100 50">
                 <polygon points="50, 10 30, 45 70, 45" stroke="#FFFFFF" />
             </svg>
         )
         return (
             <svg viewBox="0 0 100 50">
-                <rect x="30" y="5" width="40" height="40" stroke="#FFFFFF" />
+                <rect x="33" y="5" width="35" height="35" stroke="#FFFFFF" />
             </svg>
         )
     }
@@ -33,18 +38,18 @@ function Team() {
                 <div className="xl:text-3xl xs:text-2xl mt-20 text-xl max-w-8xl lg:space-y-8 space-y-4 font-courier">
                     <div className="team font-bold text-3xl">
                         Team
-                </div>
-                    <div className="grid lg:grid-cols-3 grid-cols-1 xl:gap-x-40 gap-x-20 mt-4 text-xl">
-                        {TeamMembers.map((member, index) => {
-                            return (
-                                <div className="flex flex-col text-center my-5 items-center" key={index}>
-                                    {polygon(index)}
-                                    <p className="font-bold py-2 md:text-2xl text-xl">{member.title}</p>
-                                    <p className="font-normal xs:text-lg text-sm py-2">{member.desc}</p>
-                                </div>
-                            )
-                        })}
                     </div>
+                </div>
+                <div className="grid lg:grid-cols-2 grid-cols-1 xl:gap-x-40 gap-x-20 mt-4 text-xl">
+                    {TeamMembers.map((member, index) => {
+                        return (
+                            <div className="flex flex-col text-center my-5 items-center" key={index}>
+                                {polygon(index)}
+                                <p className="font-bold py-2 md:text-2xl text-xl">{member.title}</p>
+                                <p className="font-normal xs:text-lg text-sm py-2">{member.desc}</p>
+                            </div>
+                        )
+                    })}
                 </div>
             </div>
         </motion.section>
--- src/pages/components/TeamMembers.js
+++ src/pages/components/TeamMembers.js
@@ -5,13 +5,18 @@ export const TeamMembers = [
         desc: 'Ex-Head of Vis. Dev. at Universal Pictures. 20+ big-screen credits (Transformers 2-7, The Avengers, Thor, The Wolverine, and Terminator Genisys). Currently heads a studio, working with Boston Dynamics, Netflix, and Riot Games.',
     },
     {
+        title: 'appleseed',
+        url: '#',
+        desc: 'Ex-Blizzard concept artist. Worked on some games you’ve probably stayed up until 4am playing. Currently doodling new places and faces.',
+    },
+    {
         title: 'chromedoves',
         url: '#',
-        desc: 'Ex-Goldman Sachs investment banker. Playing with bubbles since I was 5. Have fun with it – otherwise, what\'s the point?',
+        desc: 'Ex-Goldman Sachs investment banker. Might be able to get reservations at Dorsia. If it’s not fun, don’t do it. Been playing with bubbles since I was 5.',
     },
     {
         title: 'akilwrath',
         url: '#',
-        desc: 'Ex-FAANG, Unicorn Engineer. Used to building systems, currently building a vibe. Mom does not approve.',
+        desc: 'Ex-FAANG and startup Software Engineer. Used to building systems, currently building a vibe. Mom does not approve.',
     },
 ]
