235313df7060c93c835e31bb083acb72aa770927
s01rice <s01rice@hotmail.ca>
2022-04-30 03:57:15 -0400

    fixed nav

--- src/pages/components/Layout.js
+++ src/pages/components/Layout.js
@@ -19,7 +19,7 @@ function Header(props) {
         this.props.handleClick(isOpen);
     }
 
-    return <div className="absolute w-full text-center h-20 px-8 pt-8 z-50">
+    return <div className="absolute w-full text-center h-20 px-8 pt-8 z-40">
         <div className="flex h-full items-center justify-center max-w-[1920px] mx-auto">
             <Logo />
             <Nav />
